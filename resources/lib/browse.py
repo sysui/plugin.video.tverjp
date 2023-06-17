8c5a723eb89ef3b6b27295194d1708380a6363f3
sysui <99@eeu.jp>
2023-06-17 03:25:49 +0000

    change the endpoint URI of this addon

--- resources/lib/browse.py
+++ resources/lib/browse.py
@@ -1,8 +1,8 @@
-import datetime
+from datetime import datetime
 import json
 import re
 import sys
-from typing import NamedTuple
+from typing import TypedDict
 from urllib.parse import quote_plus
 from urllib.parse import urlencode
 from urllib.request import build_opener
@@ -16,58 +16,51 @@ import requests
 
 
 def show_top():
-    add_action(name="総合ランキング", action="show_ranking_all")
-    add_action(name="ドラマランキング", action="show_ranking_drama")
-    add_action(name="バラエティランキング", action="show_ranking_variety")
-    add_action(name="すべて 新着", action="show_newer_all")
-    add_action(name="ドラマ 新着", action="show_newer_drama")
-    add_action(name="バラエティ 新着", action="show_newer_variety")
-    add_action(name="検索", action="show_keyword_search")
+    add_dashboard()
+    listitem = xbmcgui.ListItem("検索")
+    url = "%s?action=%s" % (sys.argv[0], "show_keyword_search")
+    xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, True)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def show_ranking_all():
-    url = "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/all"
-    data = get(url, {"x-tver-platform-type": "web"})
-    contents = data["result"]["contents"]["contents"]
-    add_contents(contents)
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+def add_dashboard():
+    dashboard_endpoints = [
+        {
+            "name": "総合ランキング",
+            "url": "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/all",
+        },
+        {
+            "name": "ドラマランキング",
+            "url": "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/drama",
+        },
+        {
+            "name": "バラエティランキング",
+            "url": "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/variety",
+        },
+        {
+            "name": "すべて 新着",
+            "url": "https://service-api.tver.jp/api/v1/callNewerDetail/all",
+        },
+        {
+            "name": "ドラマ 新着",
+            "url": "https://service-api.tver.jp/api/v1/callNewerDetail/drama",
+        },
+        {
+            "name": "バラエティ 新着",
+            "url": "https://service-api.tver.jp/api/v1/callNewerDetail/variety",
+        },
+    ]
+    for dashboard_endpoint in dashboard_endpoints:
+        listitem = xbmcgui.ListItem(dashboard_endpoint["name"])
+        url = "%s?action=%s&dashboard_url=%s" % (
+            sys.argv[0],
+            "dashboard",
+            quote_plus(dashboard_endpoint["url"]),
+        )
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, True)
 
 
-def show_ranking_drama():
-    url = "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/drama"
-    data = get(url, {"x-tver-platform-type": "web"})
-    contents = data["result"]["contents"]["contents"]
-    add_contents(contents)
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-def show_ranking_variety():
-    url = "https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/variety"
-    data = get(url, {"x-tver-platform-type": "web"})
-    contents = data["result"]["contents"]["contents"]
-    add_contents(contents)
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-def show_newer_all():
-    url = "https://service-api.tver.jp/api/v1/callNewerDetail/all"
-    data = get(url, {"x-tver-platform-type": "web"})
-    contents = data["result"]["contents"]["contents"]
-    add_contents(contents)
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-def show_newer_drama():
-    url = "https://service-api.tver.jp/api/v1/callNewerDetail/drama"
-    data = get(url, {"x-tver-platform-type": "web"})
-    contents = data["result"]["contents"]["contents"]
-    add_contents(contents)
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-def show_newer_variety():
-    url = "https://service-api.tver.jp/api/v1/callNewerDetail/variety"
+def dashboard(url: str):
     data = get(url, {"x-tver-platform-type": "web"})
     contents = data["result"]["contents"]["contents"]
     add_contents(contents)
@@ -75,13 +68,12 @@ def show_newer_variety():
 
 
 def __url(url: str) -> tuple[str, str]:
-    buf = urlread(url)
-    episode = json.loads(buf)
-    video = episode.get("video")
+    episode = get(url)
+    video = episode["video"]
     videoRefID = video.get("videoRefID")
     videoID = video.get("videoID")
-    accountID = video.get("accountID")
-    playerID = video.get("playerID")
+    accountID = video["accountID"]
+    playerID = video["playerID"]
     url = f"https://players.brightcove.net/{accountID}/{playerID}_default/index.min.js"
     buf = urlread(url)
     policykey = re.search(
@@ -116,7 +108,7 @@ def play(url: str):
     xbmcplugin.setResolvedUrl(int(sys.argv[1]), succeeded=True, listitem=listitem)
 
 
-class ContentInfo(NamedTuple):
+class ContentInfo(TypedDict):
     id: str
     version: int
     title: str
@@ -132,50 +124,32 @@ class ContentInfo(NamedTuple):
     productionProviderName: str
 
 
-class Content(NamedTuple):
+class Content(TypedDict):
     type: str
     content: ContentInfo
 
 
-def add_contents(contents):
+def add_contents(contents: list[Content]):
     items = []
     for data in contents:
-        item = data.get("content")
-        id = item.get("id")
-        date = item.get("broadcastDateLabel")
-        title = []
-        if date:
-            title.append(date)
-        if item.get("seriesTitle"):
-            title.append(item.get("seriesTitle"))
-        if item.get("title"):
-            title.append("    " + item.get("title"))
-        title = " ".join(title)
+        item = data["content"]
+        id = item["id"]
+        title = f'{item["broadcastDateLabel"]} {item["seriesTitle"]}  {item["title"]}'
         description = "\n".join(
             [
                 item["seriesTitle"],
                 item["title"],
+                "",
                 item["broadcastDateLabel"],
                 "配信終了: "
-                + datetime.datetime.fromtimestamp(item["endAt"]).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
+                + datetime.fromtimestamp(item["endAt"]).strftime("%Y-%m-%d %H:%M:%S"),
                 "字幕: " + str(item["isSubtitle"]),
                 item["broadcasterName"],
             ]
         )
-        broadcasterName = item.get("broadcasterName")
         thumbnail = (
             f"https://statics.tver.jp/images/content/thumbnail/episode/small/{id}.jpg"
         )
-        episode_url = f"https://statics.tver.jp/content/episode/{id}.json"
-        labels = {
-            "title": title,
-            "plot": description,
-            "plotoutline": description,
-            "studio": broadcasterName,
-            "date": __labeldate(__date(date)),
-        }
         listitem = xbmcgui.ListItem(title)
         listitem.setArt(
             {
@@ -184,43 +158,18 @@ def add_contents(contents):
                 "poster": thumbnail,
             }
         )
+        labels = {
+            "title": title,
+            "plot": description,
+            "plotoutline": description,
+            "studio": item["broadcastDateLabel"],
+        }
         listitem.setInfo(type="video", infoLabels=labels)
         listitem.setProperty("IsPlayable", "true")
+        episode_url = f"https://statics.tver.jp/content/episode/{id}.json"
         url = "%s?action=%s&url=%s" % (sys.argv[0], "play", quote_plus(episode_url))
         items += [(url, listitem, False)]
     xbmcplugin.addDirectoryItems(int(sys.argv[1]), items)
-
-
-def add_action(name, action):
-    listitem = xbmcgui.ListItem(name)
-    url = "%s?action=%s" % (sys.argv[0], action)
-    xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, True)
-
-
-def __date(itemdate):
-    now = datetime.datetime.now()
-    year0 = now.strftime("%Y")
-    date0 = now.strftime("%m-%d")
-    date = "0000-00-00"
-    m = re.match(r"(20[0-9]{2})年", itemdate)
-    if m:
-        date = "%s-00-00" % (m.group(1))
-    m = re.match(r"([0-9]{1,2})月([0-9]{1,2})日", itemdate)
-    if m:
-        date1 = "%02d-%02d" % (int(m.group(1)), int(m.group(2)))
-        date = "%04d-%s" % (int(year0) - 1 if date1 > date0 else int(year0), date1)
-    m = re.match(r"([0-9]{1,2})/([0-9]{1,2})", itemdate)
-    if m:
-        date1 = "%02d-%02d" % (int(m.group(1)), int(m.group(2)))
-        date = "%04d-%s" % (int(year0) if date1 < date0 else int(year0) - 1, date1)
-    return date
-
-
-def __labeldate(date):
-    m = re.search("^([0-9]{4})-([0-9]{2})-([0-9]{2})", date)
-    if m:
-        date = "%s.%s.%s" % (m.group(3), m.group(2), m.group(1))
-    return date
 
 
 def urlread(url: str, *headers):
